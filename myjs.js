e0fa999913e1532bec89fdb1f38296f29e9fb55f
zyoshiki1110 <zm23441@josai.ac.jp>
2025-07-01 10:34:49 +0900

    Update myjs.js

--- myjs.js
+++ myjs.js
@@ -1,7 +1,2 @@
-alert(Math.sqrt(1+1));
-alert((2+3));
-alert('2'+'3');
-alert(Number('2')+Number('3'));
-alert('2'+3);
-alert(NUmber('2'+3));
+alert(Math.sqrt(1+1)(2+3)('2'+'3')(Number('2')+Number('3')(Number('2')+Number('3')('2'+3)(NUmber('2'+3));
 
